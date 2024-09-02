8c6a16fd539f241b2b3c5b4a53a42b94a3bdcbd8
mitanshu <mitanshubadgujar8@gmail.com>
2024-09-02 20:42:03 +0530

    jkkj

---
+++ frontend/src/data/footer-links.js
@@ -0,0 +1,35 @@
+export const FooterLink2 = [
+  {
+    title: "Subjects",
+    links: [
+      
+      { title: "Mobile Development", link: "#" },
+      { title: "Web Design", link: "#" },
+      { title: "Web Development", link: "#" },
+    ],
+  },
+  {
+    title: "Languages",
+    links: [
+
+      { title: "C++", link: "#" },
+ 
+
+      { title: "HTML & CSS", link: "#" },
+
+      { title: "JavaScript", link: "#" },
+   
+    ],
+  },
+  {
+    title: "Career building",
+    links: [
+        {title: "Career paths", link: "#"},
+        {title: "Career services", link: "#"},
+        {title: "Interview prep", link: "#"},
+        {title: "Professional certification", link: "#"},
+     
+      
+    ]
+  }
+];
